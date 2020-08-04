767f7d97ea4d2ab3dec81f9e815a248919d7b2ce
Devin Black <Devinjamesblack@gmail.com>
2020-08-04 07:50:40 -0600

    converting from years to seconds to seconds to years

--- answers/spaceAge/main.go
+++ answers/spaceAge/main.go
@@ -20,26 +20,26 @@ const (
 func secondsToAge(p Planet, age float64) float64 {
 
 	if p == earth {
-		age *= 60. * 60. * 24. * 365.25
+		age /= 60. / 60. / 24. / 365.25
 	} else if p == mercury {
-		age *= 60. * 60. * 24. * 88
+		age /= 60. / 60. / 24. / 88
 	} else if p == venus {
-		age *= 60. * 60. * 24. * 225
+		age /= 60. / 60. / 24. / 225
 	} else if p == mars {
-		age *= 60. * 60. * 24. * 687
+		age /= 60. / 60. / 24. / 687
 	} else if p == jupiter {
-		age *= 60. * 60. * 24. * 4300
+		age /= 60. / 60. / 24. / 4300
 	} else if p == saturn {
-		age *= 60. * 60. * 24. * 11000
+		age /= 60. / 60. / 24. / 11000
 	} else if p == uranus {
-		age *= 60. * 60. * 24. * 31000
+		age /= 60. / 60. / 24. / 31000
 	} else if p == neptune {
-		age *= 60. * 60. * 24. * 60200
+		age /= 60. / 60. / 24. / 60200
 	}
 	return age
 }
 
 func main() {
 
-	fmt.Printf("This is how old I would be on Earth in seconds %f", secondsToAge(mercury, 24))
+	fmt.Printf("How old I would be from seconds to years on any planet %f", secondsToAge(earth, 1000000000))
 }
